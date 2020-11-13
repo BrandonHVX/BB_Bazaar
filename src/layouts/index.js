53be6f05d568f6a39240193e5b2388f2fca07717
Brandon <brandongines@gmail.com>
2020-11-12 22:32:50 -0500

    updated

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StaticQuery, graphql } from "gatsby";
 import Header from "../components/header";
 import HeaderOne from "../wrappers/header/HeaderOne";
-import Navi from "../components/Navi";
+
 import Footer from "../components/footer";
 import Provider from "../context/provider";
 import "./layout.scss";
